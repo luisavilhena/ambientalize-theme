527bd6643271fb09e70750dcca7a3d2fced0cab8
luisavilhena <luisavilhena@gmail.com>
2021-11-11 20:07:17 -0300

    apaga excesso

--- blocks/load.php
+++ blocks/load.php
@@ -5,18 +5,4 @@
 	require('testimonial.php');
 	require('text-img.php');
 
-
-
-	require('rich-text.php');
-	require('image-columns.php');
-	require('one-image.php');
-	require('text-box.php');
-	require('social-media.php');
-	require('columns-image-text-box.php');
-
-
-	require('title_img.php');
-	require('text-link-list.php');
-	require('gallery.php');
-	require('menu-color.php');
 ?>
